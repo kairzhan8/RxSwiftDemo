bf8d91b742ce7621be67f1e9400d8ad2512b5e59
kairzhan <kairzhankural@gmail.com>
2021-02-23 15:48:57 +0600

    applying the filter to the image

--- RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/ViewController.swift
@@ -11,6 +11,7 @@ import RxSwift
 class ViewController: UIViewController {
     
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var applyFilterButton: UIButton!
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
@@ -18,13 +19,29 @@ class ViewController: UIViewController {
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+    @IBAction func applyFilterButtonPressed() {
+        guard let sourceImage = photoImageView.image else { return }
+        FilterService().applyFilter(to: sourceImage) { (filteredImage) in
+            DispatchQueue.main.async {
+                self.photoImageView.image = filteredImage
+            }
+        }
+    }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let navC = segue.destination as? UINavigationController,
               let photosVC = navC.viewControllers.first as? PhotosCollectionViewController else { fatalError("Segue destination is not found")}
         photosVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
-            self?.photoImageView.image = photo
+            DispatchQueue.main.async {
+                self?.updateUI(with: photo)
+            }
         }).disposed(by: disposeBag)
+    }
+    
+    private func updateUI(with image: UIImage) {
+        photoImageView.image = image
+        applyFilterButton.isHidden = false
     }
 
 }
